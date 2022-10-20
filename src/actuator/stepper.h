e5c49b933d1b29c759740689ae831a8671f40b2b
Alex <alextremo677@yahoo.com>
2022-10-20 00:00:05 -0500

    Final

--- src/actuator/stepper.h
+++ src/actuator/stepper.h
@@ -4,38 +4,8 @@
 #include <stdint.h>
 
 #include <entities.h>
-#include <AccelStepper.h>
 
 #include <Servo.h>
-
-class StepperActuador: public AccelStepper, public Actuator {
-private:
-public:
-    StepperActuador(
-        uint8_t pin1, 
-        uint8_t pin2, 
-        /*
-        uint8_t pin3, 
-        uint8_t pin4, 
-        */
-        bool enable = true
-    ):AccelStepper(
-        /*
-        AccelStepper::FULL4WIRE,
-        pin1, 
-        pin2, 
-        pin3, 
-        pin4, 
-        enable
-        */
-    //Se cambia la configuracion para usar controlador
-       AccelStepper::DRIVER,
-        pin1, 
-        pin2, 
-        enable
-    ), Actuator(){};
-    void execute(Position);
-};
 
 class ServoActuator: public Servo{
 private:
@@ -46,11 +16,6 @@ public:
     };
     void execute(Position);
 };
-
-
-
-
-void StepperActuador::execute(Position pos){}
 
 void ServoActuator::execute(Position pos){}
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,8 @@
 #include <Arduino.h>
 #include <user_interfaces/potenciometer.h>
 #include <actuator/stepper.h>
-#include <user_interfaces/button.h>
 #include <llp.h>
 
-#define BUTTON_INPUT 0x09
 long timer = millis();
 
 #define servo1_ID 0xA6
@@ -12,25 +10,17 @@ long timer = millis();
 
 PotentiometerInput pot = PotentiometerInput(A0);
 PotentiometerInput pot2 = PotentiometerInput(A1);
-StepperActuador stepper = StepperActuador(1,2, true);
 ServoActuator servo1 = ServoActuator(9);
 ServoActuator servo2 = ServoActuator(10);
-//Button button = Button(BUTTON_INPUT, false, 100);
 
 void setup(){
   servo1.attach(9);
   servo2.attach(10);
-  pinMode(BUTTON_INPUT, OUTPUT);
   Serial.begin(115200);
 }
 void loop(){
     Position pos = pot.GetPosition();
     Position pos2 = pot2.GetPosition();
-    stepper.execute(pos);
-    //Serial.println(pos.getValue());
-    //servo1.write(pos.getValue()); 
-    //servo2.write(pos2.getValue()); 
-
 
     DataPack payload;
     payload.addData(
@@ -45,6 +35,4 @@ void loop(){
     while (Serial.available()) {
         Serial.read();
     }
- 
-
 }
